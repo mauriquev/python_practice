1f4cbad5c5b19d441a19d439cedf870d3448aab1
Mauricio Quevedo <mauriqg@gmail.com>
2025-02-18 21:04:26 -0500

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# This Repo is to store my practice python code
+
+**Goals:**
+- Get better at Python
+- Use Python as primary language
+- Create DevOps automation using Python
+- Build apps to deploy in my cluster
+
---
+++ print_folder_files.py
@@ -0,0 +1,32 @@
+import os 
+
+# loop through the list of folders
+# check if the folder exists
+# if the folder does not exist, print an error message
+def list_files_folder (folder):
+    try:
+        files = os.listdir(folder)
+        return files, None
+    except FileNotFoundError:
+        return None, "Folder does not exist"
+    except PermissionError:
+        return None, "You do not have permission to access the folder" 
+                                 
+# ask the user to enter the folder names
+# split the folder names into a list
+def main():
+    folders = input ("Please enter the folder names with space in between:").split()
+
+    for folder in folders:
+        files, error_message = list_files_folder(folder)
+        if files:
+            print (f"The files in {folder}:")
+            for file in files:
+                print (file)
+        else:
+            print (f"Error in {folder}: {error_message}")
+
+if __name__== "__main__":
+    main()
+
+
